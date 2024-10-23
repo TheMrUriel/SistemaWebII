33225c45c252fbcbdf123523a62bc1e335014fa1
al180302uacj <al180302@alumnos.uacj.mx>
2024-10-22 20:48:34 -0700

    Se agrego log in

--- Transporte/TransporteApp/urls.py
+++ Transporte/TransporteApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.log_in, name = 'login'),
+    path('home/', views.home, name='home')
 ]
--- Transporte/TransporteApp/views.py
+++ Transporte/TransporteApp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'App/Inicio.html')
+
+def log_in(request):
+    return render(request, 'App/login.html')
